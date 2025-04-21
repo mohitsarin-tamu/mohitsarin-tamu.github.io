f02a8a6f0077664b4c5d3552ce4e80c93750ece3
mohitsarin-tamu <mohitsarin26@tamu.edu>
2025-04-20 23:08:39 -0500

    scripts.js update

--- scripts.js
+++ scripts.js
@@ -1,20 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const filterButtons = document.querySelectorAll('.filter-button');
-    const projectEntries = document.querySelectorAll('.project-entry');
-  
-    filterButtons.forEach(button => {
-      button.addEventListener('click', function() {
-        const filter = this.getAttribute('data-filter');
-  
-        projectEntries.forEach(entry => {
-          if (filter === 'all' || entry.getAttribute('data-category') === filter) {
-            entry.style.display = 'flex';
-          } else {
-            entry.style.display = 'none';
-          }
-        });
+  const filterButtons = document.querySelectorAll('.filter-button');
+  const projectEntries = document.querySelectorAll('.project-entry');
+
+  filterButtons.forEach(button => {
+    button.addEventListener('click', function () {
+      const filter = this.getAttribute('data-filter');
+
+      projectEntries.forEach(entry => {
+        const categories = entry.getAttribute('data-category').split(" ");
+        if (filter === 'all' || categories.includes(filter)) {
+          entry.style.display = 'flex';
+        } else {
+          entry.style.display = 'none';
+        }
       });
     });
   });
-
-
+});
