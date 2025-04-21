6a50ad931d9ce533f13093ca5af2cd05a714da21
mohitsarin-tamu <mohitsarin26@tamu.edu>
2025-04-20 23:19:31 -0500

    udpate scripts.js

--- scripts.js
+++ scripts.js
@@ -2,23 +2,23 @@ document.addEventListener('DOMContentLoaded', function () {
   const filterButtons = document.querySelectorAll('.filter-button');
   const projectEntries = document.querySelectorAll('.project-entry');
 
-  // Show all projects initially
-  projectEntries.forEach(entry => {
-    entry.style.display = 'flex';
-  });
-
   filterButtons.forEach(button => {
     button.addEventListener('click', function () {
       const filter = this.getAttribute('data-filter');
 
       projectEntries.forEach(entry => {
-        const categories = entry.getAttribute('data-category').split(" ");
-        if (filter === 'all' || categories.includes(filter)) {
+        const categories = entry.getAttribute('data-category').toLowerCase().split(' ');
+        if (filter === 'all' || categories.includes(filter.toLowerCase())) {
           entry.style.display = 'flex';
         } else {
           entry.style.display = 'none';
         }
       });
     });
+  });
+
+  // Show all on initial load
+  projectEntries.forEach(entry => {
+    entry.style.display = 'flex';
   });
 });
